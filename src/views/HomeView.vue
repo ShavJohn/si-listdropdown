95717bd348d8811d4cfd9f3278549f81cea4cbcf
ShavJohn <shavarsh.garabedian@gmail.com>
2023-06-23 14:37:54 +0400

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+
+    </div>
 </template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+
+    })
+
+</script>
